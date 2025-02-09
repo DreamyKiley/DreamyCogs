18ebbe3870ba4b7e549d9d3c11e5f16ad04eeabb
435dsf3xcv <86751611+435dsf3xcv@users.noreply.github.com>
2025-02-08 18:35:18 -0600

    Fixed redirect and bypass ssl verification errors

--- pepe/pepe.py
+++ pepe/pepe.py
@@ -12,18 +12,25 @@ class PepeCog(commands.Cog):
     @commands.cooldown(1, 5, commands.BucketType.user)  # Set a cooldown of 1 use per 5s per user
     async def get_pepe(self, ctx):
         
-        file_path = os.path.join(os.path.dirname(__file__), "pepe.txt") # You can change these to any other website, I just had it pull from archive.org to make it simpler
+        file_path = os.path.join(os.path.dirname(__file__), "pepe.txt") 
         
         try:
             with open(file_path, "r") as file:
                 pepe_images = file.readlines()
 
             pepe_images = [url.strip() for url in pepe_images if url.strip()]  # Remove any empty lines
-
+            
+            # Choose a random image
             selected_image = random.choice(pepe_images)
 
+            # Fix the URL before making the request
+            selected_image = selected_image.replace(
+                "https://archive.org/download/", "https://ia804501.us.archive.org/3/items/"
+            )
+
+            # Use SSL=False to ignore SSL certificate verification errors
             async with aiohttp.ClientSession() as session:
-                async with session.get(selected_image) as response:
+                async with session.get(selected_image, allow_redirects=True, ssl=False) as response:
                     if response.status == 200:
                         await ctx.send(selected_image)
                     else:
